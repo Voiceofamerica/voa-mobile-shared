82b491ab8969d98eb2feb71164f4fe245679785d
Kevin Groat <apps@kevingroat.io>
2017-12-06 11:33:13 -0500

    Adding MediaPlayer

---
+++ src/components/MediaPlayer/MediaPlayer.story.tsx
@@ -0,0 +1,16 @@
+
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
+
+import MediaPlayer from './MediaPlayer'
+
+storiesOf('MediaPlayer', module)
+  .addDecorator(withKnobs)
+  .add('display video', () => (
+    <MediaPlayer src='https://av.voanews.com/Videoroot/Pangeavideo/2017/12/1/14/146a3e25-ebcf-444e-b1dd-133cf64a46e4_mobile.mp4' playbackRate={number('playbackRate', 1, { min: 0.5, max: 2, step: 0.05, range: true })} />
+  ))
+  .add('play audio', () => (
+    <MediaPlayer src='https://av.voanews.com/clips/VCH/2017/12/06/39923226-d756-47fd-bdf4-b5b6523d9c1f.mp3' playbackRate={number('playbackRate', 1, { min: 0.5, max: 2, step: 0.05, range: true })} />
+  ))
---
+++ src/components/MediaPlayer/MediaPlayer.tsx
@@ -0,0 +1,57 @@
+
+import * as React from 'react'
+import { Subscription } from 'rxjs/Subscription'
+
+import { waitUntilOnline } from '../../helpers/resilience'
+
+import { mediaPlayer, mediaContent } from './MediaPlayer.scss'
+
+export interface Props {
+  src: string
+  className?: string
+  style?: React.CSSProperties
+  onLoadDone?: () => void
+  playbackRate?: number
+}
+
+class MediaPlayer extends React.Component<Props> {
+  player: HTMLVideoElement
+
+  componentWillUpdate (nextProps: Props) {
+    if (this.player) {
+      const { playbackRate = 1 } = nextProps
+      this.player.playbackRate = playbackRate
+    }
+  }
+
+  setPlayer = (player: HTMLVideoElement) => {
+    const { playbackRate = 1 } = this.props
+
+    this.player = player
+
+    if (player) {
+      player.playbackRate = playbackRate
+    }
+  }
+
+  renderContent = () => {
+    const { src } = this.props
+
+    return (<video ref={this.setPlayer} controls src={src} className={mediaContent} />)
+  }
+
+  render () {
+    const {
+      className = '',
+      style,
+    } = this.props
+
+    return (
+      <div className={`${mediaPlayer} ${className}`} style={style}>
+        { this.renderContent() }
+      </div>
+    )
+  }
+}
+
+export default MediaPlayer
