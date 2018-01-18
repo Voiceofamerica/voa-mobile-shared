b3c8cf97ed3d5d09ac1723cb028f75dc56b9b5e9
Kevin Groat <apps@kevingroat.io>
2018-01-18 09:43:30 -0500

    Fixing styles

--- src/components/MediaPlayer/MediaPlayer.tsx
+++ src/components/MediaPlayer/MediaPlayer.tsx
@@ -52,6 +52,7 @@ class MediaPlayer extends React.Component<Props> {
         onPause={() => this.triggerTogglePlay(false)}
         style={style}
         playsinline
+        playsInline
       />
     )
   }
