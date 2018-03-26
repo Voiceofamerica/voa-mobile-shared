69b4b52062e1819c1b93c73e601627b64638f42c
Kevin Groat <apps@kevingroat.io>
2018-03-26 16:13:01 -0400

    Fixed tests and coverage

---
+++ src/components/TopNav/StaticItem.spec.tsx
@@ -0,0 +1,28 @@
+
+import * as React from 'react'
+import { create } from 'react-test-renderer'
+
+import StaticItem from './StaticItem'
+
+describe('<StaticItem />', () => {
+  describe('shapshots', () => {
+    it('should render empty', () => {
+      const element = create((
+        <StaticItem />
+      ))
+
+      expect(element).toMatchSnapshot()
+    })
+
+    it('should render with the given children', () => {
+      const element = create((
+        <StaticItem>
+          <div>child1</div>
+          <div>child2</div>
+        </StaticItem>
+      ))
+
+      expect(element).toMatchSnapshot()
+    })
+  })
+})
