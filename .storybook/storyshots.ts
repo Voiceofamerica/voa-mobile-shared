850e36a9bec71e9236a6348d6465598a26b3ad77
Kevin Groat <apps@kevingroat.io>
2018-04-19 16:53:31 -0400

    Fixed storyshots

--- .storybook/storyshots.ts
+++ .storybook/storyshots.ts
@@ -1,4 +1,12 @@
 
 import initStoryshots from '@storybook/addon-storyshots'
 
+jest.mock('react-dom', () => {
+  return {
+    render: () => null,
+    unmountComponentAtNode: () => null,
+    findDOMNode: () => { return {} },
+  }
+})
+
 initStoryshots()
