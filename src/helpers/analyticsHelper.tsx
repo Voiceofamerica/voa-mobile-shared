146fff6041e766bd73ac1fc5c8ee4d3661e8e7a5
Kevin Groat <apps@kevingroat.io>
2018-04-25 13:58:55 -0400

    Updated analytics helper

--- src/helpers/analyticsHelper.tsx
+++ src/helpers/analyticsHelper.tsx
@@ -4,94 +4,145 @@ import * as React from 'react'
 import { deviceIsReady } from './cordovaHelper'
 import { startObservable } from './psiphonHelper'
 
-export let ADB: AdbInterface
+const PROXY_ON: ADB.ProxyStatus = 'proxy_on'
+const PROXY_OFF: ADB.ProxyStatus = 'proxy_off'
 
-if (typeof (cordova as any) !== 'undefined') {
-  ADB = require('adobe-mobile-services/sdks/Cordova/ADBMobile/Shared/ADB_Helper')
-}
-
-export interface AppAnalyticsOptions {
-  language: string
-}
-
-let analyticsOptions: AppAnalyticsOptions | undefined = undefined
-
-export function setAnalyticsOptions (opts: AppAnalyticsOptions) {
-  analyticsOptions = opts
-}
-
-export type ProxyStatus = 'proxy_on' | 'proxy_off'
-const PROXY_ON: ProxyStatus = 'proxy_on'
-const PROXY_OFF: ProxyStatus = 'proxy_off'
-
-export interface TrackStateOptions {
-  language: string
-  section: string
-  content_type: string
-  page_title: string
-  page_name: string
-  article_uid?: string
-  audio_name?: string
-  video_name?: string
-  proxy_status: ProxyStatus
-}
-
-export interface TrackActionOptions {
-  language: string
-  content_type: string
-  page_title: string
-  page_name: string
-  section?: string
-  category?: string
-  headline?: string
-  article_uid?: string
-  proxy_status: ProxyStatus
-}
+const mediaNames: ADB.MediaName[] = [
+  'audio',
+  'video',
+]
 
 const getProxyStatus = () => {
   return startObservable.getValue() ? PROXY_ON : PROXY_OFF
 }
 
-export interface AdbInterface {
-  trackState<T> (path: string, options: TrackStateOptions & T)
-  trackAction<T> (type: string, options: TrackActionOptions & T)
+const baseStateOptions: ADB.BaseTrackStateOptions = {
+  language: '', // Set from analytics options
+  language_service: '', // Set from analytics options
+  platform: 'news app',
+  entity: 'voa',
+  property_name: '', // Set from analytics options
+  property_id: '', // Set from analytics options
+  app_type: 'hybrid',
+  get proxy_status () { return getProxyStatus() },
+  rsid_acct: '', // Set from analytics options
 }
 
-export function trackState<T> (path: string, options: TrackStateOptions & T) {
-  return deviceIsReady.then(() => {
-    ADB && ADB.trackState(path, options)
+const baseActionOptions: ADB.BaseTrackActionOptions = {
+  language: '', // Set from analytics options
+  language_service: '', // Set from analytics options
+  platform: 'news app',
+  entity: 'voa',
+  property_name: '', // Set from analytics options
+  property_id: '', // Set from analytics options
+  app_type: 'hybrid',
+  get proxy_status () { return getProxyStatus() },
+  report_suite: '', // Set from analytics options
+}
+
+const articleToActionOptions = (opts: ArticleActionOpts): ADB.GeneratedTrackActionOptions => ({
+  page_name: opts.articleTitle,
+  content_type: 'article',
+  section: '???',
+  category: '???',
+  page_title: opts.articleTitle,
+  headline: opts.articleTitle,
+  byline: opts.authors,
+  pub_date: opts.pubDate,
+  article_uid: opts.id,
+  app_events: '???',
+})
+
+export const adbReady = deviceIsReady.then(() => {
+  const ADB: ADB.AdbInterface = require('adobe-mobile-services/sdks/Cordova/ADBMobile/Shared/ADB_Helper')
+  return ADB
+})
+
+let optionsConfigured: () => void
+export const analyticsOptionsReady = new Promise(resolve => {
+  optionsConfigured = resolve
+})
+
+export interface AppAnalyticsOptions {
+  language: string
+  languageService: string
+  propertyName: string
+  propertyId: string
+  rsidAccount: string
+  reportSuite: string
+}
+
+export function setAnalyticsOptions (opts: AppAnalyticsOptions) {
+  baseActionOptions.language
+  = baseStateOptions.language
+  = opts.language
+
+  baseActionOptions.language_service
+  = baseStateOptions.language_service
+  = opts.languageService
+
+  baseActionOptions.property_name
+  = baseStateOptions.property_name
+  = opts.propertyName
+
+  baseActionOptions.property_id
+  = baseStateOptions.property_id
+  = opts.propertyId
+
+  baseStateOptions.rsid_acct = opts.rsidAccount
+
+  baseActionOptions.report_suite = opts.reportSuite
+
+  optionsConfigured()
+}
+
+export function trackState (type: string, options: ADB.GeneratedTrackStateOptions) {
+  const fullOptions = {
+    ...baseStateOptions,
+    ...options,
+  }
+
+  if (dataLayer) {
+    dataLayer.push(fullOptions)
+    dataLayer.push({ appScreenView: type })
+  }
+
+  return adbReady.then((ADB) => {
+    ADB.trackState(type, fullOptions)
   })
 }
 
-export function trackAction<T> (type: string, options: TrackActionOptions & T) {
-  return deviceIsReady.then(() => {
-    ADB && ADB.trackAction(type, options)
+export function trackAction (type: string, options: ADB.GeneratedTrackActionOptions) {
+  const fullOptions = {
+    ...baseActionOptions,
+    ...options,
+  }
+
+  if (dataLayer) {
+    dataLayer.push(fullOptions)
+    dataLayer.push({ appEvent: type })
+  }
+
+  return adbReady.then((ADB) => {
+    ADB.trackAction(type, fullOptions)
   })
 }
 
-export function favoriteArticle (opts: { id: string, articleTitle: string, authors: string }) {
-  return trackAction('ARTICLE_TO_FAV', {
-    language: analyticsOptions!.language,
-    content_type: 'article',
-    article_uid: opts.id,
-    page_title: opts.articleTitle,
-    page_name: opts.articleTitle,
-    headline: opts.articleTitle,
-    byline: opts.authors,
-    proxy_status: getProxyStatus(),
-  })
+export interface ArticleActionOpts {
+  id: string,
+  articleTitle: string,
+  authors: string,
+  pubDate: string,
 }
 
-export function shareArticle (opts: { id: string, articleTitle: string, authors: string }) {
+export function favoriteArticle (opts: ArticleActionOpts) {
+  return trackAction('ARTICLE_TO_FAV', articleToActionOptions(opts))
+}
+
+export function shareArticle (opts: ArticleActionOpts & { shareType: string }) {
   return trackAction('ARTICLE_SHARED', {
-    language: analyticsOptions!.language,
-    content_type: 'article',
-    article_uid: opts.id,
-    page_title: opts.articleTitle,
-    page_name: opts.articleTitle,
-    headline: opts.articleTitle,
-    byline: opts.authors,
-    proxy_status: getProxyStatus(),
+    ...articleToActionOptions(opts),
+    share_type: opts.shareType,
   })
 }
 
@@ -104,15 +155,23 @@ export interface AnalyticsProps {
   analytics: typeof analyticsHelper
 }
 
+export type ItemType = 'article' | 'audio' | 'video'
+
 export interface HOCAnalyticsOptions {
+  skip?: boolean
+  itemType?: ItemType
   state: string
   title: string
+  contentType?: ADB.StateContentType
   section?: string
-  type?: string
-  skip?: boolean
+  byline?: string
+  pubDate?: string
+  articleId?: string | number
+  searchKeyword?: string
+  mediaType?: ADB.MediaType
 }
 
-function getVal<P> (item: HOCAnalyticsOptions | ((props: Readonly<P>, prevProps: Readonly<P>) => HOCAnalyticsOptions), props: P, prevProps: P): HOCAnalyticsOptions {
+function getVal<P> (item: HOCAnalyticsOptions | ((props: Readonly<P>, prevProps: Readonly<P>) => HOCAnalyticsOptions), props: Readonly<P>, prevProps: Readonly<P>): HOCAnalyticsOptions {
   if (typeof item === 'object') {
     return item
   } else if (typeof (item as any) === 'function') {
@@ -126,48 +185,11 @@ export default function analytics<P = {}> (options: HOCAnalyticsOptions | ((prop
   return function (Component: React.ComponentType<P & AnalyticsProps>): React.ComponentType<P> {
     return class AnalyticsComponent extends React.Component<P & Partial<AnalyticsProps>> {
       componentDidMount () {
-        const {
-          state,
-          title,
-          section = 'listing',
-          type = 'index',
-          skip = false,
-        } = getVal(options, this.props, {})
-
-        if (skip) {
-          return Promise.resolve()
-        }
-
-        return trackState(state, {
-          language: analyticsOptions!.language,
-          section: section,
-          content_type: type,
-          page_title: title,
-          page_name: title,
-          proxy_status: getProxyStatus(),
-        })
+        this.fireState(getVal(options, this.props, {})).catch()
       }
 
-      componentWillReceiveProps (nextProps: Readonly<P & Partial<AnalyticsProps>>) {
-        const {
-          state,
-          title,
-          section = 'listing',
-          type = 'index',
-          skip = false,
-        } = getVal(options, nextProps, this.props)
-        if (skip) {
-          return Promise.resolve()
-        }
-
-        return trackState(state, {
-          language: analyticsOptions!.language,
-          section: section,
-          content_type: type,
-          page_title: title,
-          page_name: title,
-          proxy_status: getProxyStatus(),
-        })
+      componentDidUpdate (prevProps: Readonly<P & Partial<AnalyticsProps>>) {
+        this.fireState(getVal(options, this.props, prevProps)).catch()
       }
 
       render () {
@@ -176,6 +198,43 @@ export default function analytics<P = {}> (options: HOCAnalyticsOptions | ((prop
             {this.props.children}
           </Component>
         )
+      }
+
+      private fireState = (options: HOCAnalyticsOptions) => {
+        const {
+          skip = false,
+          itemType,
+          state,
+          title,
+          contentType = 'index',
+          section = 'listing',
+          byline,
+          pubDate,
+          articleId,
+          searchKeyword,
+          mediaType,
+        } = options
+
+        if (skip) {
+          return Promise.resolve()
+        }
+
+        return trackState(state, {
+          page_name: title,
+          video_name: itemType === 'video' ? title : undefined,
+          audio_name: itemType === 'audio' ? title : undefined,
+          content_type: contentType,
+          section: section,
+          category: section,
+          page_title: title,
+          headline: itemType === 'article' ? title : undefined,
+          byline: byline,
+          pub_date: pubDate,
+          article_uid: `${articleId}`,
+          search_keyword: searchKeyword,
+          media_type: mediaType,
+          media_name: itemType && (itemType in mediaNames) ? itemType as ADB.MediaName : undefined,
+        })
       }
     }
   }
