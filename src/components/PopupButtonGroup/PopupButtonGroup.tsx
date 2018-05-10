df269723fdfac54ff73a08df8453b38d6e89d874
Kevin Groat <apps@kevingroat.io>
2018-05-09 21:33:52 -0400

    Updates to popup button group to animate position

--- src/components/PopupButtonGroup/PopupButtonGroup.tsx
+++ src/components/PopupButtonGroup/PopupButtonGroup.tsx
@@ -6,6 +6,7 @@ import { themed, ThemeProps, DEFAULT_THEME } from '../ThemeProvider'
 import {
   popupButtonGroup,
   show,
+  visible,
 } from './PopupButtonGroup.scss'
 
 export interface Props extends ThemeProps {
@@ -15,17 +16,44 @@ export interface Props extends ThemeProps {
   style?: React.CSSProperties
 }
 
+export interface State {
+  isVisible: boolean
+}
+
 @themed
-class PopupButtonGroup extends React.Component<Props> {
+class PopupButtonGroup extends React.Component<Props, State> {
+  state: State = {
+    isVisible: this.props.show || false,
+  }
+
+  timeoutId: any
+
+  componentDidUpdate (prevProps: Props) {
+    const { show = false } = this.props
+    const { isVisible } = this.state
+
+    if (show === isVisible) {
+      return
+    }
+
+    clearTimeout(this.timeoutId)
+
+    this.timeoutId = setTimeout(() => {
+      this.setState({ isVisible: show })
+    })
+  }
+
   render () {
     const { verticalPosition = 0, show: isShown = false, className = '', style = {}, theme = DEFAULT_THEME, children } = this.props
+    const { isVisible } = this.state
     const {
       popupButtonBackground: background,
     } = theme
 
-    const fullClassName = isShown
-                        ? `${popupButtonGroup} ${show} ${className}`
-                        : `${popupButtonGroup} ${className}`
+    const showClass = isShown ? show : ''
+    const visibleClass = isVisible ? visible : ''
+
+    const fullClassName = `${popupButtonGroup} ${showClass} ${visibleClass} ${className}`
 
     return (
       <div className={fullClassName} style={{ background, top: verticalPosition, ...style }}>
